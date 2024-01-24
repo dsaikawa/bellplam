43af995117df67a15e0d6313cd3c5b2998ab7689
宰川大介 <daisuke@saikawadaisukenoMacBook-Pro.local>
2024-01-24 23:04:59 +0900

    コンタクトページを追加

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,5 +1,42 @@
+import { Button, Container, Stack, Text, TextInput, Textarea } from '@mantine/core';
+import classes from '../../components/Header/Header.module.css';
+
 const ContactPage = () => {
-  return <></>;
+  return (
+    <Container size="sm" mt="xl">
+      <Stack justify="center">
+        <Text size="50px" className={classes.title}>
+          Contact 🫶
+        </Text>
+        <TextInput
+          size="md"
+          label="Name"
+          placeholder="Input placeholder"
+          required
+          styles={{ required: { color: 'black' } }}
+        />
+        <TextInput
+          size="md"
+          label="Email"
+          withAsterisk
+          placeholder="Input placeholder"
+          required
+          styles={{ required: { color: 'black' } }}
+        />
+        <Textarea
+          size="md"
+          label="Message"
+          withAsterisk
+          placeholder="Input placeholder"
+          required
+          styles={{ required: { color: 'black' } }}
+        />
+        <Button variant="outline" color="black" size="md">
+          Submit
+        </Button>
+      </Stack>
+    </Container>
+  );
 };
 
 export default ContactPage;
