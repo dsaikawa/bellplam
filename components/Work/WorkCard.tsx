76aeca0f4bcd462c412fd265242287fa9870b2dd
宰川大介 <daisuke@saikawadaisukenoMacBook-Pro.local>
2024-01-27 01:45:53 +0900

    画像拡大のライブラリを追加

--- components/Work/WorkCard.tsx
+++ components/Work/WorkCard.tsx
@@ -1,22 +1,21 @@
-import { Center, Text, Image, Stack, Badge, UnstyledButton } from '@mantine/core';
+import { Center, Text, Image, Stack, Badge, UnstyledButton, Anchor } from '@mantine/core';
 import { useElementSize } from '@mantine/hooks';
 
 type WorkCardProps = { image: string; link: string; title: string; category: string };
 const WorkCard = ({ image, link, title, category }: WorkCardProps) => {
   const { ref, width, height } = useElementSize();
   return (
-    <>
+    <Anchor href="/work/1">
       <Image src={image} fit="cover" ref={ref} h={width / 1.618} />
       <UnstyledButton mt={'3'}>
         <Badge variant="outline" color="black" size="sm" radius="xl">
           {category}
         </Badge>
       </UnstyledButton>
-      <Text tt="capitalize" mt={'3'}>
+      <Text tt="capitalize" mt={'3'} c="black">
         {title}
       </Text>
-    </>
-    // <Text>{title}</Text>
+    </Anchor>
   );
 };
 
--- pages/work/[id].tsx
+++ pages/work/[id].tsx
@@ -1,5 +1,7 @@
 import { Badge, Container, Grid, Image, Stack, Text, Title, UnstyledButton } from '@mantine/core';
 import { useState } from 'react';
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
 
 const imageSrc = ['/sample1.jpg', '/sample2.jpg', '/sample3.jpg'];
 
@@ -7,33 +9,45 @@ const WorkDetail = () => {
   const [open, setOpen] = useState(false);
   const [index, setIndex] = useState(0);
   return (
-    <Container size="xl" mt="xl">
-      <Stack>
-        <UnstyledButton>
-          <Badge variant="outline" color="black" size="sm" radius="xl">
-            MUSIC VIDEO
-          </Badge>
-        </UnstyledButton>
-        <Title order={2}>ふうりん。「1億分の1」official music video</Title>
-        <Stack gap="0">
-          <Text c="gray">@furin_2005</Text>
-          <Text c="gray">Starring : @__am.06_</Text>
-          <Text c="gray">Shot by Moriaki Suzuki (BELLPLAM)</Text>
-          <Text c="gray">Photographer : Yohei Umeki (BELLPLAM)</Text>
+    <>
+      <Container size="xl" mt="xl">
+        <Stack>
+          <UnstyledButton>
+            <Badge variant="outline" color="black" size="sm" radius="xl">
+              MUSIC VIDEO
+            </Badge>
+          </UnstyledButton>
+          <Title order={2}>ふうりん。「1億分の1」official music video</Title>
+          <Stack gap="0">
+            <Text c="gray">@furin_2005</Text>
+            <Text c="gray">Starring : @__am.06_</Text>
+            <Text c="gray">Shot by Moriaki Suzuki (BELLPLAM)</Text>
+            <Text c="gray">Photographer : Yohei Umeki (BELLPLAM)</Text>
+          </Stack>
+          {imageSrc.map((i, index) => {
+            return (
+              <Image
+                src={i}
+                key={index}
+                onClick={() => {
+                  setOpen(true), setIndex(index);
+                }}
+              />
+            );
+          })}
         </Stack>
-        {imageSrc.map((i, index) => {
-          return (
-            <Image
-              src={i}
-              key={index}
-              onClick={() => {
-                setOpen(true), setIndex(index);
-              }}
-            />
-          );
-        })}
-      </Stack>
-    </Container>
+      </Container>
+      {open && (
+        <Lightbox
+          mainSrc={imageSrc[index]}
+          nextSrc={imageSrc[(index + 1) % imageSrc.length]}
+          prevSrc={imageSrc[(index + imageSrc.length - 1) % imageSrc.length]}
+          onCloseRequest={() => setOpen(false)}
+          onMovePrevRequest={() => setIndex((index + imageSrc.length - 1) % imageSrc.length)}
+          onMoveNextRequest={() => setIndex((index + 1) % imageSrc.length)}
+        />
+      )}
+    </>
   );
 };
 export default WorkDetail;
